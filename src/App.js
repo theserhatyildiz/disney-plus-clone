d7787809d8bdbed945ed6763d8d77ba21a0fe63c
Serhat Yildiz <yildizserhat@yahoo.com>
2023-11-14 17:55:42 +0300

    ADD: detal page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      
+      </Router>
     </div>
   );
 }
